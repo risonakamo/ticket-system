63db9e0917bc97ab3364bc7a7932779571b216aa
risonakamo <hmykanae@gmail.com>
2022-11-23 21:08:51 -0500

    update ticket

--- src/main/java/com/team1/incidentticketsystem/models/Ticket2.java
+++ src/main/java/com/team1/incidentticketsystem/models/Ticket2.java
@@ -18,6 +18,7 @@ import com.vladmihalcea.hibernate.type.array.ListArrayType;
 
 import java.util.ArrayList;
 
+import org.apache.commons.lang3.ObjectUtils.Null;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 
 /** full ticket with all fields */
@@ -81,6 +82,89 @@ public class Ticket2 extends Ticket
         this.opened=true;
 
         this.assignedEmployees=new ArrayList<UUID>();
+    }
+
+    /** update this ticket with another. only certain fields can be changed, unless provide
+     *  admin setting, or assigned employees setting, which allows more fields to be changed. */
+    public void update(Ticket2 ticket,Boolean admin,Boolean assignedEmployee)
+    {
+        if (ticket.title!=null)
+        {
+            this.title=ticket.title;
+        }
+
+        if (ticket.type!=null)
+        {
+            this.type=ticket.type;
+        }
+
+        if (ticket.impactedSystems!=null)
+        {
+            this.impactedSystems=ticket.impactedSystems;
+        }
+
+        if (ticket.description!=null)
+        {
+            this.description=ticket.description;
+        }
+
+        if (ticket.comments!=null)
+        {
+            this.comments=ticket.comments;
+        }
+
+        if (ticket.userSeverity!=null)
+        {
+            this.userSeverity=ticket.userSeverity;
+        }
+
+        // fields that assigned employees or admins can change
+        if (admin || assignedEmployee)
+        {
+            if (ticket.status!=null)
+            {
+                this.status=ticket.status;
+            }
+
+            if (ticket.opened!=null)
+            {
+                this.opened=ticket.opened;
+            }
+
+            if (ticket.lastUpdateTime!=null)
+            {
+                this.lastUpdateTime=ticket.lastUpdateTime;
+            }
+
+            if (ticket.stepsTaken!=null)
+            {
+                this.stepsTaken=ticket.stepsTaken;
+            }
+
+            if (ticket.severity!=null)
+            {
+                this.severity=ticket.severity;
+            }
+        }
+
+        // fields that only admin can change
+        if (admin)
+        {
+            if (ticket.creatorId!=null)
+            {
+                this.creatorId=ticket.creatorId;
+            }
+
+            if (ticket.assignedEmployees!=null)
+            {
+                this.assignedEmployees=ticket.assignedEmployees;
+            }
+
+            if (ticket.creationDate!=null)
+            {
+                this.creationDate=ticket.creationDate;
+            }
+        }
     }
 
     @Override
